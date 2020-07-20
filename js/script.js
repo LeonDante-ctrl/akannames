620bc03f858e349420350bc01c3ca4abe5f53250
Dante.L <dantel@D.L>
2020-07-20 07:41:00 +0300

    manage to find an if else algorithm that works with the codes intended purpose

---
+++ js/script.js
@@ -0,0 +1,57 @@
+// array months , weeks and names with linking as wriitten in your notepad
+
+
+function calc(){
+
+  function akanName (weekday, gender){
+    // male highlighting
+      if (weekDay === 0 && gender === "male"){
+        return ("Kwasi")
+      }
+      if (weekDay === 1 && gender === "male"){
+        return ("Kwadwo")
+      }
+      if (weekDay === 2 && gender === "male"){
+        return ("Kwabena")
+      }
+      if (weekDay === 3 && gender === "male"){
+        return ("Kwaku")
+      }
+      if (weekDay == 4 && gender === "male"){
+        return ("Yaw")
+      }
+      if (weekDay === 5 && gender === "male"){
+        return ("Kofi")
+      }
+      if (weekDay === 6 && gender === "male"){
+        return ("Kwame")
+      }
+      //check female names
+      if (weekDay === 0 && gender === "female"){
+        return ("Akosua")
+      }
+      if (weekDay === 1 && gender === "female"){
+        return ("Adwoa")
+      }
+      if (weekDay === 2 && gender === "female"){
+        return ("Abenaa")
+      }
+      if (weekDay === 3 && gender === "female"){
+        return ("Akua")
+      }
+      if (weekDay === 4 && gender === "female"){
+        return ("Yaa")
+      }
+      if (weekDay === 5 && gender === "female"){
+        return ("Afua")
+      }
+      if (weekDay === 6 && gender === "female"){
+        return ("Ama")
+      }
+      //LIMIT APPLICATION
+      else {
+        return(alert("INVALID DATE! Please use 'MM/DD/YYYY' format"));
+      }
+    }
+
+    
